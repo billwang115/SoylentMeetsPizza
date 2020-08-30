077ef07b9f43a71510c89d9365946a39412c0070
William Wang <billwang115@gmail.com>
2020-08-30 01:19:16 -0400

    profile page

--- soylentmeetspizza/src/components/Profile.vue
+++ soylentmeetspizza/src/components/Profile.vue
@@ -1,6 +1,47 @@
 <template>
 <div>
-    lol
+    <v-card class="mx-auto" max-width="434">
+        <v-img height="100%" src="https://news.efinancialcareers.com/binaries/content/gallery/efinancial-careers/articles/2018/11/Big-data-1.jpg">
+            <v-row align="end" class="fill-height">
+                <v-col align-self="start" class="pa-10" cols="6">
+                    <v-avatar class="profile" color="grey" size="164">
+                        <v-img src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p280x280/118323130_607124523266748_3172322240217469177_n.jpg?_nc_cat=103&_nc_sid=b96e70&_nc_ohc=0iKk_Qmzj-4AX9iNAH9&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&tp=6&oh=04d34449853573506f9bb58044ae68b4&oe=5F7089F8"></v-img>
+                    </v-avatar>
+                </v-col>
+                <v-col class="py-16">
+                    <v-list-item color="rgba(0,0,0,.4)" dark>
+                        <v-list-item-content>
+                            <v-list-item-title class="title">Alvin Zhang</v-list-item-title>
+                            <v-list-item-subtitle>Data Scientist</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-col>
+            </v-row>
+        </v-img>
+    </v-card>
+
+    <v-card class="mx-auto" width="434">
+        <v-card-text class="text--primary">
+      <div>Aspiring Data scientist. Interested in data science applications in Sports.</div>
+      <br>
+      <div>Expertise Area:</div>
+      <div>
+        <ul>
+          <li>Back-end</li>
+          <li>Machine Learning</li>
+        </ul>
+      </div>
+        <br>
+      <div>
+          <v-chip color="purple lighten-1" filter><a href="https://github.com/billwang115">GitHub</a></v-chip>
+          <v-chip color="purple lighten-1" filter style="margin-left: 10px; margin-right:10px;"><a href="https://www.linkedin.com/in/billwang115/">LinkedIn</a><br></v-chip>
+      </div>
+      <br>
+      <div>
+          <v-chip  class="ma-2" color="blue lighten-1" large> Edit </v-chip>
+      </div>
+    </v-card-text>
+    </v-card>    
 </div>
 </template>
 
